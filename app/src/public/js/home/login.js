067d4e69293977435b8e7812e6425f3e01175fb1
vennylee <hyungsup42134@gmail.com>
2022-01-30 00:50:17 +0900

    DOM객체 제어를 통해 로그인 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,23 @@
 "use strict";
 
-console.log("hello world");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+  loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+  const req = {
+    id: id.value,
+    psword: psword.value,
+  };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  });
+  // POST 요청을 받기위한 API 설정이 필요하다(app.post) like app.get
+}
