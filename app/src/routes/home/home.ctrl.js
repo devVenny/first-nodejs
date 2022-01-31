468631d07c904ecf5ee02acba9708197abcd6fb5
vennylee <hyungsup42134@gmail.com>
2022-01-31 23:55:31 +0900

    회원가입 라우팅 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -19,23 +19,11 @@ const process = {
     const user = new User(req.body);
     const response = user.login();
     return res.json(response);
-    // return res.json(response);
-    // const id = req.body.id;
-    // const psword = req.body.psword;
-    // const users = UserStorage.getUsers("id", "psword");
-
-    // if (users.id.includes(id)) {
-    //   const idx = users.id.indexOf(id);
-    //   if (users.psword[idx] === psword) {
-    //     return res.json({
-    //       success: true, // success : ture 라는 객체를 json으로 만들어서 res값으로 넘긴다.
-    //     });
-    //   }
-    // }
-    // return res.json({
-    //   success: false,
-    //   msg: "로그인에 실패했습니다.",
-    // });
+  },
+  register: (req, res) => {
+    const user = new User(req.body);
+    const response = user.register();
+    return res.json(response);
   },
 };
 
