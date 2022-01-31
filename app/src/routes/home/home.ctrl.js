c5107bb92f9ee9f2c147dd96864eeecff22ffd6d
vennylee <hyungsup42134@gmail.com>
2022-01-31 22:16:53 +0900

    유저 모델 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const UserStorage = require("../../models/UserStorage");
+const User = require("../../models/User");
 
 const output = {
   home: (req, res) => {
@@ -13,22 +13,26 @@ const output = {
 
 const process = {
   login: (req, res) => {
-    const id = req.body.id;
-    const psword = req.body.psword;
-    const users = UserStorage.getUsers("id", "psword");
+    const user = new User(req.body);
+    const response = user.login();
+    return res.json(response);
+    // return res.json(response);
+    // const id = req.body.id;
+    // const psword = req.body.psword;
+    // const users = UserStorage.getUsers("id", "psword");
 
-    if (users.id.includes(id)) {
-      const idx = users.id.indexOf(id);
-      if (users.psword[idx] === psword) {
-        return res.json({
-          success: true, // success : ture 라는 객체를 json으로 만들어서 res값으로 넘긴다.
-        });
-      }
-    }
-    return res.json({
-      success: false,
-      msg: "로그인에 실패했습니다.",
-    });
+    // if (users.id.includes(id)) {
+    //   const idx = users.id.indexOf(id);
+    //   if (users.psword[idx] === psword) {
+    //     return res.json({
+    //       success: true, // success : ture 라는 객체를 json으로 만들어서 res값으로 넘긴다.
+    //     });
+    //   }
+    // }
+    // return res.json({
+    //   success: false,
+    //   msg: "로그인에 실패했습니다.",
+    // });
   },
 };
 
