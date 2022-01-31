606da59e1fefdd17f2a24d945c46c3e1174eedd1
vennylee <hyungsup42134@gmail.com>
2022-02-01 03:22:46 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -14,16 +14,28 @@ class UserStorage {
     return userInfo;
   }
 
-  static getUsers(...fields) {
-    // field(id, psword)의 값만 담은 새로운 배열을 만든다 . name 제외.
-    // const users = this.#users;
-    const newUser = fields.reduce((newUsers, field) => {
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+    if (isAll) {
+      return users;
+    }
+    const newUsers = fields.reduce((newUsers, field) => {
       if (users.hasOwnProperty(field)) {
         newUsers[field] = users[field];
       }
       return newUsers;
     }, {});
-    return newUser;
+    return newUsers;
+  }
+
+  static getUsers(isAll, ...fields) {
+    // field(id, psword)의 값만 담은 새로운 배열을 만든다 . name 제외.
+    return fs
+      .readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUsers(data, isAll, fields);
+      })
+      .catch((err) => console.error(err));
   }
 
   static getUserInfo(id) {
@@ -35,11 +47,15 @@ class UserStorage {
       .catch((err) => console.error(err));
   }
 
-  static save(userInfo) {
-    // const users = this.#users;
+  static async save(userInfo) {
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      throw "중복된 아이디입니다.";
+    }
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.psword.push(userInfo.psword);
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
     return { success: true };
   }
 }
